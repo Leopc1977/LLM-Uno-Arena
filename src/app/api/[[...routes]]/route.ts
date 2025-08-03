3a2dafc77260c177a2535fd74b260b5aee74779f
Leopc1977 <46353973+Leopc1977@users.noreply.github.com>
2025-08-03 22:06:08 +0200

    Add new dependencies for BoundaryML and Elysia, update Next.js configuration, and implement basic game logic for Uno

--- src/app/api/[[...routes]]/route.ts
+++ src/app/api/[[...routes]]/route.ts
@@ -1,12 +1,42 @@
 import { Elysia, t } from 'elysia'
+import { b } from '../../../../baml_client'
 
 const app = new Elysia({ prefix: '/api' })
+    .onRequest(({ request }) => {
+        console.log(`[${request.method}] ${request.url}`)
+    })
     .get('/', () => 'hello Next')
+    .get('/ollama', async ({ query }) => {
+        const { api, prompt } = query
+        const response = await fetch('http://localhost:11434/api/chat', {
+            method: 'POST',
+            body: JSON.stringify({
+                model: api,
+                messages: [
+                    {
+                        role: 'user',
+                        content: prompt
+                    }
+                ],
+                stream: false,
+                temperature: 1.1
+            })
+        })
+        return response.json().then(data => data.message.content)
+    })
+    .get('/baml', async ({ query }) => {
+        const { prompt } = query
+    
+        const response = await b.ExtractResume(prompt);
+        return response;
+    })
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String()
         })
     })
 
-export const GET = app.handle 
+export const GET = app.handle
 export const POST = app.handle 
+
+export type App = typeof app
